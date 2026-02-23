7c30c08641d3de72cd6518c934e4556f96cf08e0
Emanuele Piemonte <epiemonte1@gmail.com>
2026-02-23 10:23:19 +0100

    invitea

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import MyNav from "./components/MyNav";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <MyNav />
+      </div>
+    </>
+  );
+}
+
+export default App;
