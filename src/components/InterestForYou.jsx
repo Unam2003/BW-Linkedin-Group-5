7367d9d3b6fd2374c24f0be6e7cf9ab28fb8ff11
Samuel-Valentini <samuelvitalia@gmail.com>
2026-02-23 18:39:26 +0100

    all done

--- src/components/InterestForYou.jsx
+++ src/components/InterestForYou.jsx
@@ -1,5 +1,51 @@
-const InterestForYou = () => {
-    return <div></div>;
+import { Button } from "react-bootstrap";
+
+const InterestForYou = (props) => {
+    return (
+        <div className="d-flex" style={{ gap: "0.8rem" }}>
+            <div>
+                <img
+                    style={{ width: "40px" }}
+                    className="rounded-1"
+                    src={props.imgSrc}
+                    alt={props.title}
+                />
+            </div>
+            <div>
+                <div className="fw-semibold">{props.title}</div>
+                <div style={{ fontSize: "0.8rem" }}>{props.subtitle}</div>
+                <div
+                    className="text-body-tertiary"
+                    style={{ fontSize: "0.8rem" }}>
+                    {props.follower} follower
+                </div>
+                <div>
+                    <Button
+                        variant="outline-secondary"
+                        size="sm"
+                        className="rounded-pill px-3 d-inline-flex align-items-center gap-1 mt-3"
+                        onClick={props.onClick}>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            id="add-small"
+                            fill="black"
+                            aria-hidden="true"
+                            data-supported-dps="16x16"
+                            viewBox="0 0 16 16"
+                            data-token-id="86"
+                            width="16"
+                            height="16"
+                            className=""
+                            role="img"
+                            aria-label="">
+                            <path d="M14 9H9v5H7V9H2V7h5V2h2v5h5z"></path>
+                        </svg>
+                        <span className="fw-semibold"> Segui</span>
+                    </Button>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default InterestForYou;
--- src/components/PagesForYouForBar.jsx
+++ src/components/PagesForYouForBar.jsx
@@ -1,31 +1,46 @@
-import { Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import InterestForYou from "./InterestForYou";
+import { ArrowRight } from "react-bootstrap-icons";
 
 const PagesForYouForBar = () => {
     return (
         <Card className=" rounded-4 border-1  border-light-subtle">
-            <Card.Body className="p-4">
-                <InterestForYou
-                    title="INPS_Official"
-                    subtitle="Pubblica amministrazione"
-                    imgSrc="/advertising/inps_official_logo.jpg"
-                    targetSrc="/"
-                    follower="583.571"
-                    onClick={() => console.log("segui INPS_Official")}
-                />
+            <Card.Body className="pt-4 pb-0">
+                <h6 className="m-0">Potrebbero interessarti</h6>
+                <span
+                    className="text-body-tertiary"
+                    style={{ fontSize: "0.9rem" }}>
+                    Pagine per te
+                </span>
+                <div className="pt-3">
+                    <InterestForYou
+                        title="INPS_Official"
+                        subtitle="Pubblica amministrazione"
+                        imgSrc="/advertising/inps_official_logo.jpg"
+                        targetSrc="/"
+                        follower="583.571"
+                        onClick={() => console.log("segui INPS_Official")}
+                    />
 
-                <hr className="mb-4 mt-3 text-body-tertiary" />
+                    <hr className="mb-4 mt-3 text-body-tertiary" />
 
-                <InterestForYou
-                    title="Poste Italiane"
-                    imgSrc="/advertising/poste_italiane_logo.jpg"
-                    targetSrc="/"
-                    subtitle="Trasporti, logistica"
-                    follower="583.571"
-                    onClick={() => console.log("segui Poste Italiane")}
-                />
+                    <InterestForYou
+                        title="Poste Italiane"
+                        imgSrc="/advertising/poste_italiane_logo.jpg"
+                        targetSrc="/"
+                        subtitle="Trasporti, logistica, supply chain e stoccaggio"
+                        follower="583.571"
+                        onClick={() => console.log("segui Poste Italiane")}
+                    />
+                </div>
             </Card.Body>
-            <hr className="mb-4 mt-3 text-body-tertiary" />
+            <hr className="m-0 mt-3 text-body-tertiary" />
+            <Button
+                variant="link"
+                className="fw-semibold w-100 py-2 text-decoration-none d-flex justify-content-center align-items-center gap-2 border-top text-black"
+                onClick={() => console.log("mostra tutto")}>
+                Mostra tutto <ArrowRight />
+            </Button>
         </Card>
     );
 };
