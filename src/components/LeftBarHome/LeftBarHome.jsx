e981d6b9ef9004663a9c879c2bbb47750c02c7a9
Samuel-Valentini <samuelvitalia@gmail.com>
2026-02-24 12:03:35 +0100

    spatial adjustments

---
+++ src/components/LeftBarHome/LeftBarHome.jsx
@@ -0,0 +1,5 @@
+const LeftBarHome = (props) => {
+    return <div style={{ width: props.size }}>Ciao</div>;
+};
+
+export default LeftBarHome;
--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -3,24 +3,26 @@ import ProfileMain from "./main/profile/ProfileMain";
 import MyFooter from "./MyFooter";
 
 const ProfilePage = function () {
-  return (
-    <>
-      <div className="d-flex mx-5 my-4">
-        <div className="me-4 flex-grow-1">
-          {/* MAIN (centro) */}
-          <ProfileMain />
-          <div className="d-block d-lg-none">
-            <RightBar size="100%" />
-          </div>
-        </div>
+    return (
+        <>
+            <div className="d-flex mx-5 my-4 justify-content-center">
+                <div className="me-4 ">
+                    {/* MAIN (centro) */}
+                    <div style={{ maxWidth: "792px" }}>
+                        <ProfileMain />
+                    </div>
+                    <div className="d-block d-lg-none">
+                        <RightBar size="100%" />
+                    </div>
+                </div>
 
-        <div className="d-none d-lg-block">
-          <RightBar size="304px" />
-        </div>
-      </div>
-      <MyFooter />
-    </>
-  );
+                <div className="d-none d-lg-block">
+                    <RightBar size="304px" />
+                </div>
+            </div>
+            <MyFooter />
+        </>
+    );
 };
 
 export default ProfilePage;
